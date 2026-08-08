98ddb6fe79dfc849fee1f326c166ac9980cba065
Tanseychuong <nyangchuongtiutiu@gmail.com>
2026-08-08 18:22:06 +0000

    Fix: urls.py changed

--- farm/urls.py
+++ farm/urls.py
@@ -2,6 +2,17 @@ from django.urls import path
 from django.contrib.auth.decorators import login_required
 from . import views
 
+
 urlpatterns = [
-    path('', login_required(views.DashboardView.as_view()), name='dashboard'),
+    path(
+        '',
+        login_required(views.DashboardView.as_view()),
+        name='dashboard'
+    ),
+
+    path(
+        'farms/',
+        login_required(views.FarmListView.as_view()),
+        name='farm-list'
+    ),
 ]
--- farm/views.py
+++ farm/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView
 from .models import Farm, Crop, Worker, Sale
+
 
 
 class DashboardView(TemplateView):
@@ -19,3 +20,27 @@ class DashboardView(TemplateView):
         context['worker_count'] = Worker.objects.count()
         context['recent_sales'] = Sale.objects.select_related('customer').order_by('-sale_date')[:5]
         return context
+
+
+
+class DashboardView(TemplateView):
+    template_name = 'farm/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['farm_count'] = Farm.objects.count()
+        context['crop_count'] = Crop.objects.count()
+        context['worker_count'] = Worker.objects.count()
+        context['recent_sales'] = (
+            Sale.objects
+            .select_related('customer')
+            .order_by('-sale_date')[:5]
+        )
+        return context
+
+
+class FarmListView(ListView):
+    model = Farm
+    template_name = 'farm/farm_list.html'
+    context_object_name = 'farms'
+    ordering = ['farm_name']
